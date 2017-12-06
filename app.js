602ea656725ef915992e30db78bc87a0d780d355
thestrayed <strayedpeople@gmail.com>
2017-12-06 12:46:54 +0700

    change to json format

--- app.js
+++ app.js
@@ -19,24 +19,7 @@ app.set('port', process.env.PORT || 3000);
 //#region webhook router
 router.route('/')
     .post((req, res, next) => {
-        console.log('req.body:', req.body);
-        console.log(`webhookEvent: ${req.body.webhookEvent}`);
-        console.log(`issue_event_type_name: ${req.body.issue_event_type_name}`);
-        console.log(`user: ${req.body.user}`);
-        console.log(`issue: ${req.body.issue}`);
-        console.log(`issue fields: ${req.body.issue.fields}`);
-        console.log(`issue fields issuetype: ${req.body.issue.fields.issuetype}`);
-        console.log(`issue fields project: ${req.body.issue.fields.project}`);
-        console.log(`issue fields watches: ${req.body.issue.fields.watches}`);
-        console.log(`issue fields priority: ${req.body.issue.fields.priority}`);
-        console.log(`issue fields status: ${req.body.issue.fields.status}`);
-        console.log(`issue fields aggregateprogress: ${req.body.issue.fields.aggregateprogress}`);
-        console.log(`issue fields progress: ${req.body.issue.fields.progress}`);
-        console.log(`issue fields comment: ${req.body.issue.fields.comment}`);
-        console.log(`issue fields votes: ${req.body.issue.fields.votes}`);
-        console.log(`changelog: ${req.body.changelog}`);
-        console.log(`changelog items: ${req.body.changelog.items}`);
-        
+        console.log('request body from jira: %j', req.body);
         return res.status(httpStatus.OK);
     })
 //#endregion
